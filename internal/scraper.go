32c0fc49a1367049ac4854ab29879733928e79e2
BulizhnikGames <ksmolin06@gmail.com>
2024-10-16 00:13:18 +0300

    second commit

---
+++ internal/scraper.go
@@ -0,0 +1,8 @@
+package internal
+
+import (
+	"github.com/gotd/td/telegram"
+	"github.com/gotd/td/telegram/auth"
+	"github.com/gotd/td/telegram/updates"
+	"github.com/gotd/td/tg"
+)
--- main.go
+++ main.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"context"
+	"database/sql"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/google/uuid"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"log"
+	"os"
+)
+
+func main() {
+
+}
