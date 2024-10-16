63b250c12dfa1213b64e3b23cf2c65394dad57a6
Bu1izhnik <ksmolin06@gmail.com>
2024-10-16 14:12:16 +0300

    bot.go

--- main.go
+++ main.go
@@ -1,16 +1,12 @@
 package main
 
 import (
-	"context"
-	"database/sql"
-	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
-	"github.com/google/uuid"
-	"github.com/joho/godotenv"
+	"github.com/BulizhnikGames/subbot/internal/config"
 	_ "github.com/lib/pq"
-	"log"
-	"os"
 )
 
 func main() {
+	config.Load()
+	cfg := config.Get()
 
 }
